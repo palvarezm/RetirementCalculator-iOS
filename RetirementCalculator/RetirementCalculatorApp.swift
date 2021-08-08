269caf44614005217f5dceec736618d6454e29fc
Paul <palvareze7@gmail.com>
2021-08-08 18:18:08 -0500

    Add appcenter values & gitignore

--- RetirementCalculator/RetirementCalculatorApp.swift
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct RetirementCalculatorApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        AppCenter.start(withAppSecret: "e0e45cc9-825a-44b0-b3f1-20ca39c8c404", services:[
+          Analytics.self,
+          Crashes.self
+        ])
+    }
 
     var body: some Scene {
         WindowGroup {
